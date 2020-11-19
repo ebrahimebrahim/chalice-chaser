7efba681636aa6842c33d810ef65e7056637534f
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-11-19 03:15:37 -0500

    minor improvements to Shader encapsulation and documentation

--- include/Shader.h
+++ include/Shader.h
@@ -10,12 +10,14 @@
 using namespace std::literals::string_literals;
 
 class Shader {
+
+        /** The shader program's ID in OpenGL */
+        GLuint shader_program_id = 0;
+    
     public:
 
-        //The shader program's ID in OpenGL
-        GLuint shader_program_id = 0;
 
-        // Throws runtime errors when files not found or there are compiling/linking issues
+        /** Throws runtime errors when files not found or there are compiling/linking issues */
         Shader(std::string vertex_shader_path, std::string fragment_shader_path);
         Shader() = default;
         Shader(const Shader&) = delete; // Do not copy. It manages a global OpenGL resource.
@@ -25,9 +27,11 @@ class Shader {
         ~Shader();
 
 
-        // Tell OpenGL that we are now using this shader
+        /** Tell OpenGL that we are now using this shader */
         void use() const;
 
+        /** Note: setting uniform variables needs to be done AFTER glUseProgram on shader.
+            So make sure to use "use" method above before doing this */
         void setUniform(const char* name, const glm::mat4 & mat) const;
 };
 
