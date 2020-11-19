0648eb75ba5a83358ba302035b762411adb3c087
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-11-19 16:11:46 -0500

    ensure that Entity's have access to GameWindow

--- include/Entity.h
+++ include/Entity.h
@@ -9,7 +9,7 @@ class Entity {
     GameWindow * game_window; /** handle to game window, non-owned */
     virtual ~Entity() = 0;
 public:
-    Entity();
+    Entity(GameWindow * game_window);
     int get_id() const {return id;}
     virtual void update() = 0;
     virtual void draw() = 0;
--- src/Entity.cpp
+++ src/Entity.cpp
@@ -2,6 +2,6 @@
 
 int Entity::next_id = 0;
 
-Entity::Entity(){
+Entity::Entity(GameWindow * game_window) : game_window{game_window} {
     id = next_id++;
 }
