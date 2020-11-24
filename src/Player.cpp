44194e6ff99d2a3b0ac0bdb67061b38e50a10f43
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-11-24 06:10:58 -0500

    make collision boxes for player and wall
    But something is wrong with the collision system
    Player gets stuck on wall.

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,5 +1,12 @@
 #include <Player.h>
 
+Player::Player() {
+    collision_box = CollisionBox(
+        glm::vec3(0.0f, head_height/2.0f, 0.0f), // center
+        glm::vec3(girth, head_height/2.0f, girth)  // extents
+    );
+}
+
 void Player::update(double delta){
     move_dir = glm::vec3(move_dir[0],0,move_dir[2]);
     if (walking)
@@ -10,7 +17,7 @@ void Player::resolve_collisions() {
     if (!walking) return;
     for (const auto e : collides_with_list) {
         if (e->get_collision_box()) {
-
+            
             // either null_opt or collision normal
             auto n_opt = collision_box.value().collision_with(get_pos(), e->get_pos(), e->get_collision_box().value());
 
--- src/Wall.cpp
+++ src/Wall.cpp
@@ -3,10 +3,15 @@
 #include <glm/ext/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
 
-const float Wall::wall_height = 2.0;
+
+const float Wall::wall_height = 2.0f;
 
 Wall::Wall(GameWindow * game_window) : GraphicalEntity(game_window) {
     add_self_to_game_window();
+    collision_box = CollisionBox(
+        glm::vec3{0.5f, wall_height/2.0f, 0.5f}, // center
+        glm::vec3{0.5f, wall_height/2.0f, 0.5f}  // extents
+    );
 }
 
 GraphicsData Wall::create_graphics_data() {
