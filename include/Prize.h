acb404f35da57c5094962d00a57e95ab81d2e6f9
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-11-25 07:46:04 -0500

    move prize graphics data to static member
    This way it can be accessed by anyone who can see the Prize class

--- include/Prize.h
+++ include/Prize.h
@@ -5,11 +5,19 @@
 
 
 class Prize : public GraphicalEntity {
+    static std::optional<GraphicsData> graphics_data;
 public:
     Prize(GameWindow * game_window);
 
     GraphicsData create_graphics_data() override;
     void update(double delta) override;
+
+    /**Prize::create_graphics_data is different from the usual GraphicalEntity.
+     * The static variable `Prize::graphics_data` is created once by a function that generates it, when `get_graphics_data` is called the first time.
+     * After it's created, `get_graphics_data` will return it every time.
+     * This way the data becomes accessible to anyone who can see the `Prize` class
+     * */ 
+    static GraphicsData get_graphics_data();
 };
 
 #endif // PRIZE_H
--- src/Prize.cpp
+++ src/Prize.cpp
@@ -3,15 +3,10 @@
 #include <glm/ext/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
 
-Prize::Prize(GameWindow * game_window) : GraphicalEntity(game_window) {
-    add_self_to_game_window();
-    collision_box = CollisionBox(
-        glm::vec3{0.0f, 0.0f, 0.0f}, // center
-        glm::vec3{0.8f, 1.0f, 0.8f}  // extents
-    );
-}
 
-GraphicsData Prize::create_graphics_data() {
+std::optional<GraphicsData> Prize::graphics_data; // start as std::null_opt
+
+GraphicsData generate_prize_graphics_data() {
     GraphicsData d;
     d.vertices = {
         0.0f, 0.0f, 0.0f , // center of fan
@@ -24,9 +19,26 @@ GraphicsData Prize::create_graphics_data() {
     d.shader_choice = ShaderChoice::SHADER_DEFAULT;
     d.draw_mode = GL_TRIANGLE_FAN;
     return d;
-    
 }
 
+
+Prize::Prize(GameWindow * game_window) : GraphicalEntity(game_window) {
+    add_self_to_game_window();
+    collision_box = CollisionBox(
+        glm::vec3{0.0f, 0.0f, 0.0f}, // center
+        glm::vec3{0.8f, 1.0f, 0.8f}  // extents
+    );
+}
+
+GraphicsData Prize::create_graphics_data() {
+    return get_graphics_data();
+}
+
+GraphicsData Prize::get_graphics_data() {
+    if (!graphics_data)
+        graphics_data = generate_prize_graphics_data();
+    return *graphics_data;
+}
 
 void Prize::update(double delta) {
     rotate((TAU/3.0f) * delta, glm::vec3(0.0f,1.0f,0.0f));
