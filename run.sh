bff20a7e54f81af63550a71ea647083a260bc8e9
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-11-27 21:26:56 -0500

    add a run script

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script ensures that the game executable is run from the correct working directory,
+# so that the relative asset paths expected by the game work.
+
+EXE=build/garbanzo
+SCRIPT_DIR=`dirname $0`
+
+cd $SCRIPT_DIR
+if [ -x $EXE ]
+  then
+    $EXE
+else
+  echo "Game executable not found at $SCRIPT_DIR/$EXE"
+  echo "See README for build instructions"
+fi
